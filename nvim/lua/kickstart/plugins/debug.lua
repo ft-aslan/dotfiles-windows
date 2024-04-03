8f19d53a78aaf2ee80daea4b492872e4b2f65755
ft-aslan <fthaslncn@gmail.com>
2024-04-03 06:05:03 +0300

    working on dap plugin

--- nvim/lua/kickstart/plugins/debug.lua
+++ nvim/lua/kickstart/plugins/debug.lua
@@ -22,12 +22,59 @@ return {
 
     -- Add your own debuggers here
     'leoluz/nvim-dap-go',
+    -- virtual text for the debugger
+    {
+      'theHamsta/nvim-dap-virtual-text',
+      opts = {},
+    },
+    --neovim lua debugger
+    {
+      'jbyuki/one-small-step-for-vimkind',
+      -- stylua: ignore
+      config = function()
+        local dap = require("dap")
+        dap.adapters.nlua = function(callback, conf)
+          local adapter = {
+            type = "server",
+            host = conf.host or "127.0.0.1",
+            port = conf.port or 8086,
+          }
+          if conf.start_neovim then
+            local dap_run = dap.run
+            dap.run = function(c)
+              adapter.port = c.port
+              adapter.host = c.host
+            end
+            require("osv").run_this()
+            dap.run = dap_run
+          end
+          callback(adapter)
+        end
+        dap.configurations.lua = {
+          {
+            type = "nlua",
+            request = "attach",
+            name = "Run this file",
+            start_neovim = {},
+          },
+          {
+            type = "nlua",
+            request = "attach",
+            name = "Attach to running Neovim instance (port = 8086)",
+            port = 8086,
+          },
+        }
+      end,
+    },
   },
   config = function()
+    -- setup dap config by VsCode launch.json file
+    require('dap.ext.vscode').load_launchjs()
     local dap = require 'dap'
     local dapui = require 'dapui'
 
     require('mason-nvim-dap').setup {
+      automatic_installation = false,
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
       automatic_setup = true,
