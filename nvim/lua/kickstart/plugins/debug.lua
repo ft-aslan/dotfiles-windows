785c0f3b83ed90ab77f899fcde88b6305fb9d61a
ft-aslan <fthaslncn@gmail.com>
2024-04-06 05:50:28 +0300

    manual go debug

--- nvim/lua/kickstart/plugins/debug.lua
+++ nvim/lua/kickstart/plugins/debug.lua
@@ -131,8 +131,44 @@ return {
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
     -- Install golang specific config
-    require('dap-go').setup()
+    -- require('dap-go').setup()
 
+    dap.adapters.delve = {
+      type = 'server',
+      port = '${port}',
+      executable = {
+        command = 'dlv',
+        args = { 'dap', '-l', '127.0.0.1:${port}' },
+      },
+      options = {
+        detached = false,
+      },
+    }
+
+    -- https://github.com/go-delve/delve/blob/master/Documentation/usage/dlv_dap.md
+    dap.configurations.go = {
+      {
+        type = 'delve',
+        name = 'Debug',
+        request = 'launch',
+        program = '${file}',
+      },
+      {
+        type = 'delve',
+        name = 'Debug test', -- configuration for debugging test files
+        request = 'launch',
+        mode = 'test',
+        program = '${file}',
+      },
+      -- works with go.mod packages and sub packages
+      {
+        type = 'delve',
+        name = 'Debug test (go.mod)',
+        request = 'launch',
+        mode = 'test',
+        program = './${relativeFileDirname}',
+      },
+    }
     --icons
     for name, sign in pairs(require('custom.public').icons.dap) do
       sign = type(sign) == 'table' and sign or { sign }
